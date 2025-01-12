cb9ee17ea784ced9b6a73134f35a0943d9a76f0d
niitnkumar2003 <nitinkumarja2003@gmail.com>
2025-01-12 21:46:05 +0530

    increase time

--- FindIndefOfTValue.js
+++ FindIndefOfTValue.js
@@ -11,22 +11,22 @@ let arr = [1, 3, 5, 7, 9, 11, 13, 15]
 let target = 1;
 
 
-const findIndex=(arr,target)=>{
+var search = function(nums, target) {
     let left=0;
-    let right=arr.length-1;
-
-while(left < right){
-    let midValue=Math.floor((left+right)/2)
-    
-    if(arr[midValue] == target){
-        return midValue;
+    let right=nums.length-1;
+    let findValue
+    while(left <=right){
+        let midIndex=Math.floor((left+right)/2)
+        if(nums[midIndex]==target){
+            return midIndex;
+        }
+        if(nums[midIndex] < target){
+            left=midIndex+1;
+        }else{
+            right=midIndex-1;
+        }
     }
-    if(arr[midValue] < target){
-        left =midValue;
-    }else{
-        right --
-    }
-}
-}
+    return -1
 
+};
 console.log(findIndex(arr,target))
