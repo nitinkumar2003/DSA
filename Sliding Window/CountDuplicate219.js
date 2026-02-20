fb94d5e53fc1fb3b4520eef32cd053351763a104
nitinkumaribyte <nitinkumar@ibyteinfomatics.in>
2026-02-20 22:34:50 +0530

    II

--- Sliding Window/CountDuplicate219.js
+++ Sliding Window/CountDuplicate219.js
@@ -19,3 +19,14 @@ const containsNearbyDuplicate = (arr, k) => {
 const arr = [1, 2, 3, 1];
 const k = 3;
 console.log(containsNearbyDuplicate(arr, k)); 
+
+
+// Example 1:
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+// Example 2:
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+// Example 3:
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
