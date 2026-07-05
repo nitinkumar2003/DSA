df0beb71e4052dab2fa6a3276e1341c56ee0818b
nitinkumar2003 <nitinkumarja2003@gmail.com>
2026-07-05 20:55:16 +0530

    remove duplicate

--- linklist/MergeTwoSortedArray.js
+++ linklist/MergeTwoSortedArray.js
@@ -1,3 +1,20 @@
 var mergeTwoLists = function(list1, list2) {
     let dummy = new ListNode(0);
-}
+    let current = dummy;
+
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+
+    // Attach any remaining nodes from either list
+    current.next = list1 || list2;
+
+    return dummy.next;
+};
